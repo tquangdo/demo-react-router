9fd44f7bccd4818fe7aad5f621b9d118a18fffd6
tquangdo <trangia61@gmail.com>
2020-09-29 03:15:46 +0900

    deploy

--- src/redux/constants/ConfigConst.js
+++ src/redux/constants/ConfigConst.js
@@ -1,1 +1,1 @@
-export var API_URL = 'http://localhost:3000'
+export var API_URL = (process.env.NODE_ENV === 'production') ? `https://${process.env.REACT_APP_DOMAINNAME}:${process.env.PORT}` : 'http://localhost:3000'
