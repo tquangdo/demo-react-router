dad8432e21bf8b6475c1d7bf4477314c0274f490
tquangdo <trangia61@gmail.com>
2020-09-29 03:44:52 +0900

    deploy

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const expres = require('express')
+const cors = require('cors')
+const app = expres()
+app.use(expres.json())
+app.use(cors())
+app.use(expres.json())
+const portApp = process.env.PORT + 1 || 5000
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -8,5 +16,12 @@ server.use(middlewares)
 server.use(router)
 
 server.listen(port, () => {
-    console.log(`Port: ${port} de, server ga ugoiteiru`)
+    console.log(`Port: ${port} de, json-server ga ugoiteiru`)
+})
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(expres.static('build'))
+}
+app.listen(portApp, () => {
+    console.log(`Port: ${portApp} de, app server ga ugoiteiru`)
 })
