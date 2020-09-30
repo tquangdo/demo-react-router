eec3d0a3e6b61a8ef0ea8b0e94b90c6a898fdb49
tquangdo <trangia61@gmail.com>
2020-10-01 00:39:05 +0900

    1

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ server.listen(port, () => {
     console.log(`Port: ${port} de, json-server ga ugoiteiru`)
 })
 
-const path = require('path')
-const express = require('express')
-const app = express()
-const publicPath = path.join(__dirname, '../build')
-const portExpress = process.env.PORT - 1 || 3001
-app.use(express.static('build'))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'))
-})
-app.listen(portExpress, () => {
-    console.log(`Expres server is up with port: ${portExpress}!`)
-})
+// const path = require('path')
+// const express = require('express')
+// const app = express()
+// const publicPath = path.join(__dirname, '../build')
+// const portExpress = process.env.PORT - 1 || 3001
+// app.use(express.static('build'))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(publicPath, 'index.html'))
+// })
+// app.listen(portExpress, () => {
+//     console.log(`Expres server is up with port: ${portExpress}!`)
+// })
